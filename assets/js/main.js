a9f9fd71a23008aa292248759039d589f0f8dab4
unknown <kumar05.962013@gmail.com>
2017-09-16 22:38:27 +0530

    homepage

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+  
+/*Smooth Scroll*/
+  $(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e){
+      e.preventDefault();
+      var target=this.hash;
+      var $target= $(target);
+
+      $('html, body').animate({
+        'scrollTop': $target.offset().top
+      }, 1500 , 'swing');
+    }); 
+  });
